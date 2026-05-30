38c2515b06afd7f1e1014e85b65a9aee366b335a
Jingli-123 <jingli88669@gmail.com>
2026-05-30 21:34:00 +1000

    add forntend url

--- services/middlewear.py
+++ services/middlewear.py
@@ -3,13 +3,16 @@ import httpx
 from clerk_backend_api import Clerk
 from clerk_backend_api.security import authenticate_request
 from clerk_backend_api.security.types import AuthenticateRequestOptions
+from dotenv import load_dotenv
+
+load_dotenv()
 
 def is_signed_in(request: httpx.Request):
     sdk = Clerk(bearer_auth=os.getenv('CLERK_SECRET_KEY'))
     request_state = sdk.authenticate_request(
         request,
         AuthenticateRequestOptions(
-            authorized_parties=[ "http://localhost:3000"]
+            authorized_parties=[ os.getenv("FRONT_END_URL")]
         )
     )
     return request_state.is_signed_in
